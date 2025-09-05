c4dfa53c4ee84bc21bc2aefa6eb51c520e1e03b8
Yorick Aban <yorickaban@gmail.com>
2025-09-04 22:46:32 -0700

    Fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const target = process.env.VITE_PROXY_TARGET ?? 'http://localhost:8000';
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: { '/api': 'http://localhost:8000' } 
-  }
+  server: { proxy: { '/api': target } },
 })
+
